b643830506c747b9523a6098450a6874762dc26f
mall <mall@zetyun.com>
2025-08-20 16:59:02 +0800

    1 fixed bmpport tags schedule issue. 2 check tpi-xxx port exist before get db val.

--- neutron/services/bm_gw/common/utils.py
+++ neutron/services/bm_gw/common/utils.py
@@ -151,13 +151,17 @@ class BmgwBridge(ovs_lib.OVSBridge):
             # if not peer_name:
             #     LOG.warning(f"bmgw, clean_dead_bmports, port {port} has no peer, skip it")
             #     continue
+
+            # qvo port name: qvoXXXXXXXX-XX
             qvo_name = constants.QVO_PORT_PREFIX + bmp_name[3:]
-            # maybe bmp connect to tbr, trunk port
+            # maybe bmp connect to tbr, trunk port, tpi_name: tpi-XXXXXXXX-XX
             tpi_name = constants.TRUNK_PORT_INT_PREFIX + bmp_name[4:]
 
             # 获取 br-int 上对应 port 的 tag
             tag = self.db_get_val('Port', qvo_name, 'tag')
-            tag2 = self.db_get_val('Port', tpi_name, 'tag')
+            tag2 = 0;
+            if self.port_exists(tpi_name):
+                tag2 = self.db_get_val('Port', tpi_name, 'tag')
 
             if tag == constants.OVS_DEAD_VLAN or tag2 == constants.OVS_DEAD_VLAN:
                 external_ids = self.db_get_val('Interface', bmp_name, 'external_ids')
